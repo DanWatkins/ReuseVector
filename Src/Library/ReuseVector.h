0d56c3cea9521e0f824a422238b3a170fd36fe4a
Dan Watkins <dwatkins@valpineware.com>
2014-12-11 13:37:22 -0600

    Added documentation

--- Src/Library/ReuseVector.h
+++ Src/Library/ReuseVector.h
@@ -4,6 +4,17 @@
 #include <vector>
 #include <list>
 
+/*
+ *  @brief A sequence container very similar to std::vector.
+ *
+ *  The main difference between ReuseVector and the standard C++ vector is that
+ *  the erase operation performs in O(1) time on the container size instead of
+ *  O(n). Range erasures are still O(n) time on the number of elements erased,
+ *  but each erasure is O(1) on the size of the container instead of O(n). This
+ *  makes it very useful for systems that require data to be stored in a
+ *  contiguous block of memory for performance reasons. The order of elements is
+ *  not guaranteed.
+ */
 template <typename Type>
 class ReuseVector
 {
@@ -15,13 +26,13 @@ public:
     class Iterator;
 
     /*
-     * Returns a reference to the last element in the vector.
+	 * @returns a reference to the last element in the vector.
      * Calling this function while the vector is empty is undefined behavior.
      */
     Type& back() { return mData.back(); }
 
     /*
-     * Returns an iterator pointing to the first element in the vector.
+	 * @returns an iterator pointing to the first element in the vector.
      * If the vector is empty, the returned iterator cannot be dereferenced.
      */
     Iterator begin();
@@ -47,7 +58,9 @@ public:
     Iterator end();
 
     /*
-     *
+	 * Nulls the value at the iterator position, and then swaps it with the last
+	 * value in the container. Runs in O(1) time. Size is decremented by 1 and
+	 * capacity is unaffected.
      */
     void erase(const Iterator &position);
 
